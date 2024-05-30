3b00a1c39d9a68b60a05834968f6d1de61971154
Val Zavidnoy <vzavidnoy@centrilogic.com>
2024-05-30 10:08:25 -0400

    Initial

---
+++ NPOIEvaluation/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+using System.Diagnostics;
+using System.Reflection;
+
+
+var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+var src = Path.Combine(basePath, "Data/Funding Calc bad.xlsx");
+var target = Path.Combine(basePath, "Data/Funding Calc output.xlsx");
+
+using Stream rfs = File.OpenRead(src);
+using IWorkbook workbook = new XSSFWorkbook(rfs);
+using FileStream fs = File.Create(target);
+workbook.Write(fs, false);
+
+Process.Start(
+            new ProcessStartInfo
+            {
+                FileName = target,
+                UseShellExecute = true
+            }
+        );
